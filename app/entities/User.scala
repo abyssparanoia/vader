e593a5522935f7077dfcf8225d6d487d078e059f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-23 01:04:27 +0900

    feat: impl user entity

---
+++ app/entities/User.scala
@@ -0,0 +1,27 @@
+package entities
+
+import slick.driver.MySQLDriver.api._
+
+case class User(id: String,
+                displayName: String,
+                avatarURL: String,
+                createdAt: Long,
+                updatedAt: Long)
+
+class UserTable(tag: Tag) extends Table[User](tag, "users") {
+  def id = column[String]("id", O.PrimaryKey)
+
+  def displayName = column[String]("display_name")
+
+  def avatarURL = column[String]("avatar_url")
+
+  def createdAt = column[Long]("created_at")
+
+  def updatedAt = column[Long]("updated_at")
+
+  def * =
+    (id, displayName, avatarURL, createdAt, updatedAt) <> (User.tupled, User.unapply)
+
+}
+
+object UserQuery extends TableQuery(new UserTable(_))
