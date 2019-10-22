b610e019b4366f127a330c704806bae9627d8bfb
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-10-23 04:14:33 +0900

    fix: sql

--- mysql/sql/02-create-table.sql
+++ mysql/sql/02-create-table.sql
@@ -1,9 +1,22 @@
-CREATE TABLE vader.`posts`(
+USE `vader`;
+
+CREATE TABLE `users` (
+  `id` varchar(50) NOT NULL,
+  `display_name` varchar(36) NOT NULL,
+  `avatar_url` varchar(3000) NOT NULL,
+  `created_at` bigint(20) NOT NULL,
+  `updated_at` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `posts`(
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(50) NOT NULL,
   `title` VARCHAR(255) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
   `text` VARCHAR(10000) NOT NULL,
   `created_at` BIGINT(20) NOT NULL,
   `updated_at` BIGINT(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- mysql/sql/03-insert-rows.sql
+++ mysql/sql/03-insert-rows.sql
@@ -1,7 +1,13 @@
 USE `vader`;
 
-INSERT INTO `posts` (
-    id, title, description, text, created_at, updated_at
+INSERT INTO `users` (
+    id, display_name, avatar_url, created_at, updated_at
 )
 VALUES
-    (1, 'タイトル', '概要', '内容', ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000), ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000))
+    ('DUMMY_USER_ID', 'tester', 'https://hogehoge/icon.jpeg', ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000), ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000));
+
+INSERT INTO `posts` (
+    id, user_id, title, description, text, created_at, updated_at
+)
+VALUES
+    (1, 'DUMMY_USER_ID', 'タイトル', '概要', '内容', ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000), ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000));
